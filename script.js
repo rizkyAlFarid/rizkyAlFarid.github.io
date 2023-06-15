e49a224c33d1c7f64b4199e145e3e86dca1bec58
Rizky Al Fard <alfaridrizky69@gmail.com>
2023-06-15 18:53:33 +0700

    menambahkan animasi di hamburger menu

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const hamburger = document.querySelector('.hamburger-menu');
 hamburger.addEventListener('click', function() {
     const navbar = document.getElementById('navBar');
-    navbar.classList.toggle('showDown')
+    const lineTop = document.getElementById('atas')
+    const lineBottom = document.getElementById('bawah')
+    const lineCenter = document.getElementById('tengah')
+
+    navbar.classList.toggle('showDown');
+    lineTop.classList.toggle('satu')
+    lineCenter.classList.toggle('dua')
+    lineBottom.classList.toggle('tiga')
 })
+
